5e47b731204a23b80d23c5898e3aeae046a41fb2
ARPAN JAIN <61559256+techcoderR@users.noreply.github.com>
2022-03-12 15:56:02 +0530

    Add files via upload

---
+++ streamlit_sharing.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+st.write("""
+# Determine the Simple Stock Price Application
+### My Simple stock web Application""")
+
+# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+#define the ticker symbol
+tickerSymbol = 'MSFT'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2020-5-31', end='2022-5-31')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
+st.write(tickerData.recommendations)
